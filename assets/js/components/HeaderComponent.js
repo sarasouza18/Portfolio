df6e8e1de3c6f52a583bccbe0b8d16a1c9cc56ed
Sara Souza <sara.souza.9@hotmail.com>
2026-06-15 17:48:31 -0300

    Update HeaderComponent with new subtitle and tagline

--- assets/js/components/HeaderComponent.js
+++ assets/js/components/HeaderComponent.js
@@ -5,18 +5,50 @@ export default {
             <div class="header__profile-photo">
                 <img src="assets/img/profile.jpeg" alt="Sara Souza" class="header__image">
             </div>
+
             <h1 class="header__title">Sara Souza</h1>
-            <p class="header__subtitle">Software Engineer</p>
+
+            <p class="header__subtitle">
+                Senior Backend Engineer
+            </p>
+
+            <p class="header__tagline">
+                Distributed Systems • AWS • OpenSearch • Data Platforms
+            </p>
+
             <nav class="header__nav">
                 <ul class="header__nav-list">
                     <li class="header__nav-item">
-                        <a href="#" @click.prevent="$emit('change-section', 'home')" class="header__nav-link" :class="{ 'header__nav-link--active': activeSection === 'home' }">Home</a>
+                        <a
+                            href="#"
+                            @click.prevent="$emit('change-section', 'home')"
+                            class="header__nav-link"
+                            :class="{ 'header__nav-link--active': activeSection === 'home' }"
+                        >
+                            Home
+                        </a>
                     </li>
+
                     <li class="header__nav-item">
-                        <a href="#" @click.prevent="$emit('change-section', 'about')" class="header__nav-link" :class="{ 'header__nav-link--active': activeSection === 'about' }">About Me</a>
+                        <a
+                            href="#"
+                            @click.prevent="$emit('change-section', 'about')"
+                            class="header__nav-link"
+                            :class="{ 'header__nav-link--active': activeSection === 'about' }"
+                        >
+                            About Me
+                        </a>
                     </li>
+
                     <li class="header__nav-item">
-                        <a href="#" @click.prevent="$emit('change-section', 'contact')" class="header__nav-link" :class="{ 'header__nav-link--active': activeSection === 'contact' }">Contact</a>
+                        <a
+                            href="#"
+                            @click.prevent="$emit('change-section', 'contact')"
+                            class="header__nav-link"
+                            :class="{ 'header__nav-link--active': activeSection === 'contact' }"
+                        >
+                            Contact
+                        </a>
                     </li>
                 </ul>
             </nav>
