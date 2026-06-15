f33298683c6901e5dd9b8ab841ac3b067d00a8e1
Sara Souza <sara.souza.9@hotmail.com>
2026-06-15 18:20:59 -0300

    Update LinkedIn URL in contactContent.js

--- assets/js/components/data/contactContent.js
+++ assets/js/components/data/contactContent.js
@@ -3,7 +3,7 @@ export const contactContent = {
     phone: "+55 (84) 98106-6601",
     email: "sara.souza.9@hotmail.com",
     github: "https://github.com/sarasouza18",
-    linkedin: "https://www.linkedin.com/in/sara-souza-931337116",
+    linkedin: "https://www.linkedin.com/in/sara-souza-php-opensearch",
     contactText: "Feel free to reach out via phone or email for any questions or collaboration opportunities. You can also check out my GitHub and LinkedIn profiles below.",
   };
   
